afec4683b9f8e233a8d4000587cc29a1a5546819
Israt Jahan <israt5227@gmail.com>
2026-02-17 19:34:44 +0600

    task 1 done

---
+++ problem.js
@@ -0,0 +1,23 @@
+/* 
+1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+*/
+
+let fruits = ['apple', 'mango', 'bedana', 'lichu', 'komla'];
+console.log(fruits[3])
+
+fruits[2] = 'jambura';
+console.log(fruits)
+
+/* 
+2. Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output
+*/
+
