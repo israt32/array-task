cdbbb9734b070c3fa9fe5f7d5665d32c5605b828
Israt Jahan <israt5227@gmail.com>
2026-02-17 20:42:11 +0600

    task 4 done

--- problem.js
+++ problem.js
@@ -38,3 +38,60 @@ Use the includes method to check if the array contains a javascript book.
 
 Print a message to the console indicating whether the element is present in the array or not.
 */
+
+let books = ['harry potter', 'rapunzel', '12 dancing princess', 'programming', 'javascript']
+
+if(books.includes('javascript')){
+  console.log('the book named javascript is present in the array')
+}
+else{
+  console.log('no book here')
+}
+
+/* 
+4. Checking if it's an Array
+Instructions:
+
+Create different variables, each containing either an array or a non-array value.
+
+Now use isArray to check if each variable is an array.
+
+Print a message to the console indicating whether each variable is an array or not.
+*/
+
+
+let numbers = [2,55,62, 7, 72,444, 63];
+let name = 'cat';
+let isPassed = true;
+let umbrella = ['un', 'dos', 'tress']
+
+
+if(Array.isArray(numbers)){
+  console.log('this is an array')
+}
+else{
+  console.log('this is not an array')
+}
+
+if(Array.isArray(isPassed)){
+  console.log('this is an array')
+}
+else{
+  console.log('this is not an array')
+}
+
+if(Array.isArray(name)){
+  console.log('this is an array')
+}
+else{
+  console.log('this is not an array')
+}
+
+if(Array.isArray(umbrella)){
+  console.log('this is an array')
+}
+else{
+  console.log('this is not an array')
+}
+
+
